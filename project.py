6d49f2f053a86f53fc893a9db53eab764400bd88
Mark Rice <markmrice16@gmail.com>
2024-06-21 14:49:49 +0100

    more functions

--- project.py
+++ project.py
@@ -1,3 +1,5 @@
+import random
+
 # Call for this when you want to create a new knight
 def create_knight(knights):
 
@@ -12,19 +14,7 @@ def create_knight(knights):
     # Adds the information to the knight
     knights.append(knights_data)
 
-# Show the current knights and select one
-
-
-def select_knight(knights):
-
-    while knights_number < 1:
-
-        print("Their name is: " + knights[0])
-        knights_number += 1
-
 # Call a knight and change their data
-
-
 def change_data(knights):
 
     print("--- What would you like to update? ---")
@@ -35,24 +25,38 @@ def change_data(knights):
         selection = int(input("Select your option: "))
 
         if selection == 1:
-            if knights_number >= 0:
+            if knights_number == 0:
                 print("You have a knight!")
             knights[0] = str(input("What is their new name: "))
             print("Your knight's new name is: " + knights[0])
+            return
         else:
             print("--- Please select a valid option ---")
 
     except:
         print("--- Try Again ---")
+        change_data(knights)
+
+# Show the current knights and select one
+def select_knight(knights):
+
+    # Reset the list to print all the knights you have
+    knights_number = 0
+    print("What Knight would you like to update?\n")
+    while knights_number < int(len(knights)):
+        print(str(knights_number + 1) + "- Knight's name : " + str(knights[knights_number][0]))
+        knights_number += 1
+
+    selection = (int(input("\nSelect the Knights number: ")) - 1)
+    change_data(knights[selection])
 
 # This is the menu and we make our selections here
-
-
 def menu(knights_number):
 
     # Print the display options
     print("What do you want to do?")
     print("1: Create a new knight")
+    print("2: Update your knight")
     print("0: Exit")
 
     # Allow a selection to be tested
@@ -72,20 +76,38 @@ def menu(knights_number):
             knights_number += 1
             menu(knights_number)
 
-        elif select == 0:
-            print("Goodbye")
+        elif select == 2:
+            if int(len(knights)) == 0:
+                print("You need to create a knight first!\n")
+            else:
+                select_knight(knights)
+            menu(knights_number)
 
+        elif select == 0:
+            print("--- All your knights! ---\n")
+
+            # Reset the knights_number, to count all the knights
+            knights_number = 0
+
+            while knights_number < int(len(knights)):
+                print(str(knights_number + 1) + "- Knight's name : " + str(knights[knights_number][0]))
+                knights_number += 1
+
+            if int(len(knights)) == 0:
+                print("Wait... You have no knights! Have a number: " + str(random.randint(0,100)))
+        # Required for catching an integer
         else:
             print("--- Try Again! ---\n")
             menu(knights_number)
 
+    # We are looking for integer selection
     except:
         print("--- Try Again! ---\n")
         menu(knights_number)
-
 
 # Setting the scene
 knights_number = 0
 knights = []
 
+# Run the program
 menu(knights_number)
