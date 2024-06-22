28fdfe5d1b55710c2964acbb219659a66b22adfd
Mark Rice <markmrice16@gmail.com>
2024-06-22 23:29:55 +0100

    fomatted to pep8

--- project.py
+++ project.py
@@ -1,5 +1,6 @@
 import random
 
+
 # Call for this when you want to create a new knight
 def create_knight(knights):
 
@@ -10,33 +11,38 @@ def create_knight(knights):
 
     # Set the information up for the knight
     # Set the knights name
-    knights_data['name'] = (str(input("What is the knights name: ")))
+    knights_data["name"] = (str(input("What is the knights name: "))).capitalize()
 
     # Set the knights armour colour
-    knights_data['colour'] = (str(input("What is the colour of the knights armor: ")))
+    knights_data["colour"] = (
+        str(input("What is the colour of the knights armor: "))
+    ).lower()
 
     # Set the knights weapon of choice
-    knights_data['weapon'] = (str(input("What weapon does the knight wield?: ")))
+    knights_data["weapon"] = (
+        str(input("What weapon does the knight wield?: "))
+    ).lower()
 
     # Randomly assign the knights health points
-    knights_data['hp'] = (random.randint(70,100))
+    knights_data["hp"] = random.randint(70, 100)
 
     # Randomly assign the knights baseline damage per hit
-    knights_data['damage'] = (random.randint(10,20))
+    knights_data["damage"] = random.randint(10, 20)
 
     # Randomly assign the knights luck
-    knights_data['luck'] = (random.randint(10,24))
+    knights_data["luck"] = random.randint(10, 24)
 
     # Adds the information to the knight
     knights.append(knights_data)
 
+
 # Call a knight and change their data
 def change_data(knights):
-    #Print menu of ways to edit knight
+    # Print menu of ways to edit knight
     print("--- What would you like to update? ---")
-    print("1: Knights Name: " + str(knights['name']))
-    print("2: Colour Of Armor: " + str(knights['colour']))
-    print("3: Knights Weapon: " +str(knights['weapon']))
+    print("1: Knights Name: " + str(knights["name"]))
+    print("2: Colour Of Armor: " + str(knights["colour"]))
+    print("3: Knights Weapon: " + str(knights["weapon"]))
 
     # Allow a selection to be tested
     try:
@@ -44,30 +50,31 @@ def change_data(knights):
 
         # Rename the knight
         if selection == 1:
-            knights['name'] = str(input("What is their new name: "))
-            print("Your knight's new name is: " + str(knights['name']))
+            knights["name"] = str(input("What is their new name: "))
+            print("Your knight's new name is: " + str(knights["name"]))
             return
-        
+
         # Choose a new armor colour for knight
         elif selection == 2:
-            knights['colour'] = str(input("What is their new armour colour: "))
-            print("Your knight's new armour colour is: " + str(knights['colour']))
+            knights["colour"] = str(input("What is their new armour colour: "))
+            print("Your knight's new armour colour is: " + str(knights["colour"]))
             return
-        
+
         # Choose a new weapon for knight
         elif selection == 3:
-            knights['weapon'] = str(input("What is their new weapon: "))
-            print("Your knight's new weapon is: " + str(knights['weapon']))
+            knights["weapon"] = str(input("What is their new weapon: "))
+            print("Your knight's new weapon is: " + str(knights["weapon"]))
             return
-        
+
         # If no valid choice made
         else:
-            print("--- Please select a valid option ---")       
-    
+            print("--- Please select a valid option ---")
+
     # We are looing for an integer selection
     except:
         print("(Change Data Error)--- Try Again ---")
         change_data(knights)
+
 
 # Show the current knights and select one
 def select_knight(knights):
@@ -78,12 +85,17 @@ def select_knight(knights):
 
     # Iterate through all knights in our list to print
     while knights_number < int(len(knights)):
-        print(str(knights_number + 1) + "- Knight's name : " + str(knights[knights_number]['name']))
+        print(
+            str(knights_number + 1)
+            + "- Knight's name : "
+            + str(knights[knights_number]["name"])
+        )
         knights_number += 1
 
         # Choose the knight and pass it into change_data
-        selection = (int(input("\nSelect the Knights number: ")) - 1)
+        selection = int(input("\nSelect the Knights number: ")) - 1
         change_data(knights[selection])
+
 
 # Select two knights to have a duel with
 def select_duelists(knights):
@@ -91,29 +103,41 @@ def select_duelists(knights):
     # Reset the list to print all the knights you have
     knights_number = 0
     print("What Knight would you like to start a duel?\n")
-    
+
     # Iterate through all knights in our list to print and select first duelist
     while knights_number < int(len(knights)):
-        print(str(knights_number + 1) + "- Knight's name : " + str(knights[knights_number]['name']))
+        print(
+            str(knights_number + 1)
+            + "- Knight's name : "
+            + str(knights[knights_number]["name"])
+        )
         knights_number += 1
 
     try:
 
         # Choose the knight to be the first duelist
-        duelist_one = (int(input("\nSelect the Knights number: ")) - 1)
+        duelist_one = int(input("\nSelect the Knights number: ")) - 1
 
         # Reset the list to print all the knights you have
         knights_number = 0
-        
-        print("What Knight will " + str(knights[knights_number]['name']) + " challenge to a duel? \n")
+
+        print(
+            "What Knight will "
+            + str(knights[knights_number]["name"])
+            + " challenge to a duel? \n"
+        )
         # Iterate through all knights in our list to print
         while knights_number < int(len(knights)):
             if knights_number != duelist_one:
-                print(str(knights_number + 1) + "- Knight's name : " + str(knights[knights_number]['name']))
+                print(
+                    str(knights_number + 1)
+                    + "- Knight's name : "
+                    + str(knights[knights_number]["name"])
+                )
             knights_number += 1
-                
+
         # Choose the knight to be the second duelist
-        duelist_two = (int(input("\nSelect the Knights number: ")) - 1)
+        duelist_two = int(input("\nSelect the Knights number: ")) - 1
 
         # Start the duel with the selected duelists
         duel(duelist_one, duelist_two)
@@ -122,64 +146,94 @@ def select_duelists(knights):
         print("**--- Try Again ---")
         select_duelists(knights)
 
+
 # Start a duel between the two selected knights
 def duel(knight_one, knight_two):
-    print("\nThe duel between " + knights[knight_one]['name'] + " and " + knights[knight_two]['name'] + " begins!\n")
 
-    # Find which knight gets the iniative
-    iniative = knight_two
-    second = knight_one
-    knight_one_init = int(knights[knight_one]['luck']) * random.randint(1,20)
-    knight_two_init = int(knights[knight_two]['luck']) * random.randint(1,20)
-    if (knight_one_init >= knight_two_init):
-        iniative = knight_one
-        second = knight_two
+    # Announce the duel
+    print(
+        "\nThe duel between the "
+        + knights[knight_one]["colour"]
+        + " Knight "
+        + knights[knight_one]["name"]
+        + " and the "
+        + knights[knight_one]["colour"]
+        + " Knight "
+        + knights[knight_two]["name"]
+        + " begins!\n"
+    )
 
-    # Initialise HP variables of the knights
-    initial_hp = int(knights[knight_one]['hp']) 
-    second_hp = int(knights[knight_two]['hp'])
-    initial_dmg = int(knights[knight_one]['damage']) 
-    second_dmg = int(knights[knight_two]['damage'])
+    # Find which knight gets the iniative and attacks first
+    attacker = knights[knight_two]
+    defender = knights[knight_one]
+    if int(knights[knight_one]["luck"]) * random.randint(1, 20) > int(
+        knights[knight_two]["luck"]
+    ) * random.randint(1, 20):
+        attacker = knights[knight_one]
+        defender = knights[knight_two]
+
+    # Save the knights health points to a temporary variable,
+    # so that we are not reducing the actual hp on the knight
+    attacker_hp = int(attacker["hp"])
+    defender_hp = int(defender["hp"])
 
     # Combat loop
-    while int(initial_hp) > 0 and int(second_hp) > 0:
-        # Initial Knights attack turn
+    while int(attacker_hp) > 0 and int(defender_hp) > 0:
         # Check if attack hits or misses
-        if int(knights[iniative]['luck']) * random.randint(2,4) >= 35:
+        if int(attacker["luck"]) * random.randint(2, 4) >= 35:
             # Attack hits
             # Check if the attack is a critical hit and inflict damage
-            if int(knights[iniative]['luck']) * random.randint(1,5) >= 50:
-                second_hp -= initial_dmg * 2
-                print("The " + str(knights[iniative]['colour']) + " Knight swings with his " + str(knights[iniative]['weapon']) + " and delivers a critical hit!")
-            # Inflict normal damage to second knight
+            if int(attacker["luck"]) * random.randint(1, 5) >= 50:
+                defender_hp -= attacker["damage"] * 2
+                print(
+                    "The "
+                    + str(attacker["colour"])
+                    + " Knight swings with his "
+                    + str(attacker["weapon"])
+                    + " and delivers a critical hit!"
+                )
+            # Inflict normal damage
             else:
-                second_hp -= initial_dmg
-                print("The " + str(knights[iniative]['colour']) + " Knight swings with his " + str(knights[iniative]['weapon']) + " and hits!")
-            if (second_hp <= 0):
-                print("The " + str(knights[iniative]['colour']) + " Knight wins the duel!")
+                defender_hp -= attacker["damage"]
+                print(
+                    "The "
+                    + str(attacker["colour"])
+                    + " Knight swings with his "
+                    + str(attacker["weapon"])
+                    + " and hits!"
+                )
+            if defender_hp <= 0:
+                print(
+                    str(attacker["name"])
+                    + " the "
+                    + str(attacker["colour"])
+                    + " Knight wins the duel!\n"
+                )
                 break
         else:
             # Attack misses
-            print("The " + str(knights[iniative]['colour']) + " Knight swings with his " + str(knights[iniative]['weapon']) + " and misses!")
+            print(
+                "The "
+                + str(attacker["colour"])
+                + " Knight swings with his "
+                + str(attacker["weapon"])
+                + " and misses!"
+            )
 
-        # Second Knights attack turn
-        # Check if attack hits or misses
-        if int(knights[second]['luck']) * random.randint(2,4) >= 35:
-            # Attack hits
-            # Check if the attack is a critical hit and inflict damage
-            if int(knights[second]['luck']) * random.randint(1,5) >= 50:
-                initial_hp -= second_dmg * 2
-                print("The " + str(knights[second]['colour']) + " Knight swings with his " + str(knights[second]['weapon']) + " and delivers a critical hit!")
-            # Inflict normal damage to initial knight
-            else:
-                initial_hp -= second_dmg
-                print("The " + str(knights[second]['colour']) + " Knight swings with his " + str(knights[second]['weapon']) + " and hits!")
-            if (initial_hp <= 0):
-                print("The " + str(knights[second]['colour']) + " Knight wins the duel!")
-                break
-        else:
-            # Attack misses
-            print("The " + str(knights[second]['colour']) + " Knight swings with his " + str(knights[second]['weapon']) + " and misses!")
+        # Switch the attacker and defenders
+        # temporary health point around
+        temp_hp = defender_hp
+        defender_hp = attacker_hp
+        attacker_hp = temp_hp
+        # Switch the attacker and defender around
+        # then loop again to next round of combat
+        temp = defender
+        defender = attacker
+        attacker = temp
+
+    # Go back to main menu after fight ends
+    menu(knights_number)
+
 
 # This is the menu and we make our selections here
 def menu(knights_number):
@@ -203,7 +257,7 @@ def menu(knights_number):
 
             # Print out the Knight that was made
             print("\n--- Your Knight ---\n")
-            print("Knight's name: " + str(knights[knights_number]['name'] + "\n"))
+            print("Knight's name: " + str(knights[knights_number]["name"] + "\n"))
             knights_number += 1
             menu(knights_number)
 
@@ -218,7 +272,7 @@ def menu(knights_number):
 
         # Select two knights to enter into a duel
         elif select == 3:
-            #Check if you have at least two knights
+            # Check if you have at least two knights
             if int(len(knights)) < 2:
                 print("You need at least two knights to duel!")
             else:
@@ -234,13 +288,23 @@ def menu(knights_number):
 
             # Print out all of the knights made
             while knights_number < int(len(knights)):
-                print(str(knights_number + 1) + "- " + str(knights[knights_number]['name']) + " the " + str(knights[knights_number]['colour']) 
-                + " Knight, wielding a " + str(knights[knights_number]['weapon']))
+                print(
+                    str(knights_number + 1)
+                    + "- "
+                    + str(knights[knights_number]["name"])
+                    + " the "
+                    + str(knights[knights_number]["colour"])
+                    + " Knight, wielding a "
+                    + str(knights[knights_number]["weapon"])
+                )
                 knights_number += 1
 
             # If no knights were made
             if int(len(knights)) == 0:
-                print("Wait... You have no knights! Have a number: " + str(random.randint(0,100)))
+                print(
+                    "Wait... You have no knights! Have a number: "
+                    + str(random.randint(0, 100))
+                )
 
         # Required for catching an integer
         else:
@@ -251,6 +315,7 @@ def menu(knights_number):
     except:
         print("(EXCEPTION)--- Try Again! ---\n")
         menu(knights_number)
+
 
 # Setting the scene
 knights_number = 0
