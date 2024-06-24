73934e7f7451bbe5b38c8bbe9825c8b3a58392a6
MarkMRice <markmrice16@gmail.com>
2024-06-24 20:49:12 +0100

    cleaned up mess

--- decision.py
+++
@@ -1,8 +0,0 @@
-mark = int(input('Enter your mark: '))
-
-if mark > 74:
-    print('Congratulations! That is an awesome achievement!') 
-elif mark > 5010:
-    print('Congratulations! You have passed!')
-else:
-    print('Unfortunately, you have failed.')
--- files.py
+++
@@ -1,5 +0,0 @@
-in_file = open('Python Programming Fundamentals\\text.txt', 'r')
-for line in in_file:
-    print(in_file)
-
-  
